d4354ddfc0e76f7fc3ce65766f5636cfb45274a6
Benjaminas Visockis <59376885+theBadMusician@users.noreply.github.com>
2023-07-11 00:58:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,17 +34,17 @@ tmux source ~/.tmux.conf
 wget -c https://raw.githubusercontent.com/qboticslabs/ros_install_noetic/master/ros_install_noetic.sh && chmod +x ./ros_install_noetic.sh && ./ros_install_noetic.sh && . ~/.bashrc
 
 # Install ROS plotjuggler
-sudo apt install ros-$ROS_DISTRO-plotjuggler-ros
+sudo apt install -y --upgrade ros-$ROS_DISTRO-plotjuggler-ros
 
 # Install pyenv
-sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
+sudo apt-get install -y --upgrade build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
 
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash && pyenv install 3.10 && pyenv global 3.10 && pyenv shell 3.10
 
 # Install VS Code
-sudo apt-get install wget gpg
+sudo apt-get install -y --upgrade wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -55,7 +55,7 @@ sudo apt install code # or code-insiders
 
 # Install Docker
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install -y --upgrade ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -67,6 +67,6 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y --upgrade docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # Verify that docker is installed
 sudo docker run hello-world
