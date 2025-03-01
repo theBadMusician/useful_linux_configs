0f868e37ae816c736875648cbb5ec505797b1622
Benjaminas Visockis <59376885+theBadMusician@users.noreply.github.com>
2025-03-01 22:35:23 +0100

    Add docker setup as a separate file.

---
+++ docker_setup.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+sudo apt update
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install -y --upgrade ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Same line
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install -y --upgrade docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Verify that docker is installed
+sudo docker run hello-world
