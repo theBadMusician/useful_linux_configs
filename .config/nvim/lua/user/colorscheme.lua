da51cf3be02056664ceb2ef768af3971ea191e5a
Benjaminas Visockis <59376885+theBadMusician@users.noreply.github.com>
2026-03-03 13:02:31 +0100

    Add syntax highlighting for various code elements in the Neovim colorscheme config

--- .config/nvim/lua/user/colorscheme.lua
+++ .config/nvim/lua/user/colorscheme.lua
@@ -62,5 +62,54 @@ vim.cmd([[
   hi Normal guibg=#0a0a0a
   hi SignColumn guibg=#0a0a0a
   hi NormalFloat guibg=#0a0a0a
-]])
 
+  " --- Macros and Preprocessor (Pink & Light Blue) ---
+  " Directives like #include and #define (Pink)
+  hi @keyword.directive guifg=#c586c0
+  hi @keyword.directive.define guifg=#c586c0
+  " Macro constants and functions 
+  hi @constant.macro guifg=#4fc1ff
+  hi @function.macro guifg=#dcdcaa
+
+  " --- Functions (Yellow) ---
+  hi @function guifg=#dcdcaa
+  hi @function.call guifg=#dcdcaa
+  hi @function.builtin guifg=#dcdcaa
+
+  " --- Types and Structures (Blue & Teal) ---
+  " Built-in types like int, void, char (Blue)
+  hi @type.builtin guifg=#569cd6
+  " Custom structs, enums, and typedefs (Teal)
+  hi @type guifg=#4ec9b0
+  hi @type.definition guifg=#4ec9b0
+
+  " --- Keywords and Control Flow (Pink & Blue) ---
+  " Control flow like if, else, return, while (Pink)
+  hi @keyword.conditional guifg=#c586c0
+  hi @keyword.repeat guifg=#c586c0
+  hi @keyword.return guifg=#c586c0
+  " General keywords (Blue)
+  hi @keyword guifg=#569cd6
+
+  " --- Variables and Parameters (Light Blue) ---
+  hi @variable guifg=#9cdcfe
+  hi @variable.parameter guifg=#9cdcfe
+
+  " --- Literals (Green, Orange, Blue) ---
+  " Numbers (Light Green)
+  hi @number guifg=#b5cea8
+  hi @number.float guifg=#b5cea8
+  " Booleans (Blue)
+  hi @boolean guifg=#569cd6
+  " Strings and Chars (Orange/Rust)
+  hi @string guifg=#ce9178
+  hi @character guifg=#ce9178
+
+  " --- Punctuation and Operators (Light Gray) ---
+  hi @operator guifg=#d4d4d4
+  hi @punctuation.delimiter guifg=#d4d4d4
+  hi @punctuation.bracket guifg=#d4d4d4
+
+  " --- Comments (Green) ---
+  hi @comment guifg=#6a9955 gui=italic
+]])
